4c6b752896967b4f2539f1826f3ee44f31a3d799
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-23 15:15:02 -0600

    Rgb(E)  is now a type alias to vector.R(3, E)

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,32 @@
 pub const netpbm = @import("netpbm.zig");
 pub const vector = @import("vector.zig");
-pub const color = @import("color.zig");
 pub const ray = @import("ray.zig");
 pub const examples = @import("examples.zig");
 
-pub const R3 = vector.R3;
-pub const Rgb = color.Rgb;
 pub const Ray = ray.Ray;
+
+pub fn Rgb(Channel: type) type {
+    return vector.R(3, Channel);
+}
+
+pub fn staticAssert(comptime b: bool) void {
+    if (b) {} else @compileError("staticAssert failed");
+}
+pub fn isFloat(T: type) bool {
+    return switch (@typeInfo(T)) {
+        .float, .comptime_float => true,
+        else => false,
+    };
+}
+pub fn isInteger(T: type) bool {
+    return switch (@typeInfo(T)) {
+        .int, .comptime_int => true,
+        else => false,
+    };
+}
+pub fn isVector(T: type) bool {
+    return switch (@typeInfo(T)) {
+        .vector => true,
+        else => false,
+    };
+}
