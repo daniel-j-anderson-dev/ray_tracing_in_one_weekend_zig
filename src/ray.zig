64765b06e22750d9392ea4cdb25e1409ebd4e0bc
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-23 15:15:11 -0600

    added constructor

--- src/ray.zig
+++ src/ray.zig
@@ -1,11 +1,15 @@
 const root = @import("root.zig");
-const R3 = root.vector.R3;
+const R3 = root.vector.R(3, f64);
 
 const R = f64;
 
 pub const Ray = struct {
     origin: R3,
     direction: R3,
+
+    pub fn new(origin: R3, direction: R3) Ray {
+        return .{ .origin = origin, .direction = direction };
+    }
 
     pub fn at(self: *const @This(), t: f64) R3 {
         return self.origin.add(self.direction.scalarMultiply(t));
