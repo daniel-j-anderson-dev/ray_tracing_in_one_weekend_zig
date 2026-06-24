1571e68f5d071713a818857f8e71a42653124dcd
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-23 19:43:31 -0600

    use save_example in main

--- src/main.zig
+++ src/main.zig
@@ -4,19 +4,15 @@ const maxInt = std.math.maxInt;
 const log = std.log;
 
 const root = @import("ray_tracing_in_one_weekend");
-const examples = root.examples;
+const all_examples = root.all_examples;
+const save_example = root.save_example;
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
 
-    var buffer: [1024]u8 = undefined;
-    inline for (examples.all) |example| {
-        var file = try Io.Dir.cwd().createFile(io, example.default.path, .{});
-        defer file.close(io);
-        var writer = file.writer(io, &buffer);
-
+    inline for (all_examples) |example| {
         std.log.info("generating {s} ...", .{example.default.path});
-        try example.write(&writer.interface, .{});
+        try save_example(io, example);
         std.log.info("Done", .{});
     }
 }
